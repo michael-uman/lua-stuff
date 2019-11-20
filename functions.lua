66241c9adc6695a9f9ac6e0c4a38643f1bfbd298
Michael Uman <michael.uman@wunderbar.com>
2019-11-20 14:34:40 -0800

    Modified DisplayTable to handle associative array

--- functions.lua
+++ functions.lua
@@ -5,12 +5,21 @@ function DisplayTable(title, tab)
     local index = 1
     print(title)
     for key, value in pairs(tab) do
-        print("Key " .. key .. " Type " .. type(key))
-        
+        local dispvalue = ""
+
+        -- print("Key " .. key .. " Type " .. type(key))
+
+        if type(key) == "string" then
+            dispvalue = key .. " = " .. value
+        else
+            dispvalue = value
+        end
+
         if index ~= 1 then
             line = line .. ", "
         end
-        line = line .. value
+
+        line = line .. dispvalue
         index = index + 1
     end
     line = line .. " }"
